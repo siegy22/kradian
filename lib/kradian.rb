cca9d9da924665757eac0741eeacf59f09bef526
Yves Siegrist <Elektron1c97@gmail.com>
2017-11-16 18:52:03 +0100

    Introduce ::sync_steam_to_discord

--- lib/kradian.rb
+++ lib/kradian.rb
@@ -9,4 +9,10 @@ Dotenv.load(GEM_ROOT + "/.env")
 module Kradian
   STEAM_ID = "76561198132213543"
   DISCORD_ID = "117681046220308485"
+
+  extend self
+
+  def sync_steam_to_discord
+    Kradian::Discord.update_username(Kradian::Steam.current_name)
+  end
 end
