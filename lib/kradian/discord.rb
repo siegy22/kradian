ae4b610a41c4d10ead698a128867442fa3240cc3
Yves Siegrist <Elektron1c97@gmail.com>
2017-11-16 19:42:47 +0100

    Allow daemnoization of process

--- lib/kradian/discord.rb
+++ lib/kradian/discord.rb
@@ -7,10 +7,12 @@ module Kradian
     def update_username(username)
       bot = Discordrb::Bot.new(token: ENV["DISCORD_API_TOKEN"])
       bot.run :async
-      bot.server(ENV["DISCORD_SERVER_ID"])
-         .member(Kradian::DISCORD_ID)
-         .nick = username
-      bot.stop
+      loop do
+        bot.server(ENV["DISCORD_SERVER_ID"])
+           .member(Kradian::DISCORD_ID)
+           .nick = username
+        sleep 5 * 60 # 5 minutes
+      end
     end
   end
 end
